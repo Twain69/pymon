7e14632b06ba0541e1931685ea5e39614936b4d4
Oliver Flegler <oliver@flegler.com>
2014-08-19 23:08:15 +0200

    Fixed bug when there are no fans present (now this has to be enabled explicitly)

--- src/main/resources/hardware.py
+++ src/main/resources/hardware.py
@@ -18,34 +18,41 @@ def checkFanSpeed(config):
     except (KeyError, TypeError):
         defaultMinSpeed = 500
     
+    # Check if the fans are enabled
     try:
-        p1 = subprocess.Popen(["sensors"], stdout=subprocess.PIPE)
-        p2 = subprocess.Popen(["egrep", "^fan"], stdin=p1.stdout, stdout=subprocess.PIPE)
-        output = p2.communicate()[0]
-        fans = output.split("\n")[0:-1]
-    except (OSError):
-        notification.error(config, "Sensors not installed")
-        
-    if len(fans) == 0:
-        notification.error(config, "Could not find any fans")
-        
-    
-    for fan in fans:
-        
-        fanname = fan.split()[0]
-        speed = int(fan.split()[1])
-        fanname = fanname.split(":")[0]
-        minSpeed = defaultMinSpeed
-        
+        status = config['hardware']['fanSpeed']['status']
+    except (KeyError, TypeError):
+        status = "disabled"
+
+    if status == "enabled":    
         try:
-            for override in config['hardware']['fanSpeed']['override']:
-                if fanname in override['name']:
-                    minSpeed = override['minSpeed']
-                    break
-        except (KeyError, TypeError):
-            pass
+            p1 = subprocess.Popen(["sensors"], stdout=subprocess.PIPE)
+            p2 = subprocess.Popen(["egrep", "^fan"], stdin=p1.stdout, stdout=subprocess.PIPE)
+            output = p2.communicate()[0]
+            fans = output.split("\n")[0:-1]
+        except (OSError):
+            notification.error(config, "Sensors not installed")
+            
+        if len(fans) == 0:
+            notification.error(config, "Could not find any fans")
+            
         
-        if speed < minSpeed:
-            notification.error(config, "Fan speed to low for fan " + fanname + ". Current speed: " + str(speed) + " (minSpeed: " + str(minSpeed) + ")")
-        else:
-            notification.printVerbose("Fan speed for fan " + fanname + " currently " + str(speed) + " (minSpeed: " + str(minSpeed) + ")")
+        for fan in fans:
+            
+            fanname = fan.split()[0]
+            speed = int(fan.split()[1])
+            fanname = fanname.split(":")[0]
+            minSpeed = defaultMinSpeed
+            
+            try:
+                for override in config['hardware']['fanSpeed']['override']:
+                    if fanname in override['name']:
+                        minSpeed = override['minSpeed']
+                        break
+            except (KeyError, TypeError):
+                pass
+            
+            if speed < minSpeed:
+                notification.error(config, "Fan speed to low for fan " + fanname + ". Current speed: " + str(speed) + " (minSpeed: " + str(minSpeed) + ")")
+            else:
+                notification.printVerbose("Fan speed for fan " + fanname + " currently " + str(speed) + " (minSpeed: " + str(minSpeed) + ")")
